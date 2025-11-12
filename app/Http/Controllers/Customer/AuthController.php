5b5894e71787c8842108d9e6c172e5897d76e11d
fattyahkaylaaa <fattyahrdnkayla@gmail.com>
2025-11-12 23:09:34 +0700

    chore: initial setup after git clone

--- app/Http/Controllers/Customer/AuthController.php
+++ app/Http/Controllers/Customer/AuthController.php
@@ -1,10 +1,32 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Customer;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    //
+    public function showLoginForm()
+    {
+       
+        return view('Customer.auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'username' => 'required',
+            'password' => 'required',
+        ]);
+
+        if (Auth::attempt($request->only('username', 'password'))) {
+            return redirect('/customer/dashboard');
+        }
+
+        return back()->withErrors([
+            'loginError' => 'Username atau Password salah.',
+        ]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+use App\Http\Controllers\Customer\AuthController;
+
+Route::get('/customer/login', [AuthController::class, 'showLoginForm'])->name('customer.login');
+Route::post('/customer/login', [AuthController::class, 'login'])->name('customer.login.post');

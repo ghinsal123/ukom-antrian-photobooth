5c7fbd307631d8452bc444f997e350ab170e72bd
fattyahkaylaaa <fattyahrdnkayla@gmail.com>
2025-11-15 15:42:22 +0700

    chore: initial setup after git clone

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+<<<<<<< Updated upstream
 use App\Http\Controllers\Customer\AuthController;
 
 Route::get('/', function () {
@@ -33,3 +34,24 @@ Route::prefix('admin')->group(function () {
         return view('admin.reports.index');
     });
 });
+=======
+use App\Http\Controllers\Customer\CustomerController;
+
+
+Route::get('/customer/login', [CustomerController::class, 'showLogin'])
+    ->name('customer.login');
+
+Route::post('/customer/login', [CustomerController::class, 'login'])
+    ->name('customer.login.submit');
+
+Route::get('/customer/dashboard', [CustomerController::class, 'dashboard'])
+    ->name('customer.dashboard');
+
+    
+
+Route::get('/customer/antrian', function () {return view('Customer.antrian');})->name('customer.antrian');
+Route::get('/customer/logout', function () {return view('Customer.logout');})->name('customer.logout');
+Route::get('/customer/activity/{id}', function () {return view('Customer.detail');})->name('customer.activity.detail');
+Route::get('/customer/activity/{id}/edit', function () {return view('Customer.edit');})->name('customer.activity.edit');
+Route::get('/customer/activity/{id}/delete', function () {return view('Customer.hapus');})->name('customer.activity.delete');
+>>>>>>> Stashed changes

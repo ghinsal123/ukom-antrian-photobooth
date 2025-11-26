1368ca61233cd2cc211a1414e5daeef278154376
Ghina Salsabila <gsalsabila570@gmail.com>
2025-11-26 11:13:23 +0700

    fix: update comment AntrianController

--- app/Http/Controllers/Operator/AntrianController.php
+++ app/Http/Controllers/Operator/AntrianController.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\DB;
 class AntrianController extends Controller
 {
     /**
-     * Menampilkan daftar antrian & pencarian & paginasi
+     * Menampilkan daftar antrian & pencarian 
      */
     public function index(Request $request)
     {
